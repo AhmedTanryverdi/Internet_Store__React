7e67bc03a8b80401ea64e1570daa3a640897d105
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-04-30 03:25:48 +0300

    refactoring components

--- src/pages/auth/ui/register/Register.tsx
+++ src/pages/auth/ui/register/Register.tsx
@@ -1,40 +1,49 @@
-import React from "react";
-import { Login } from "../login/Login";
-import { UserType } from "../../../../shared/types/types";
+import React, { useState } from "react";
+import { Form } from "../../../../shared/components/form/Form";
 import "./register.scss";
 
-export const Register: React.FC<UserType> = (state): React.JSX.Element => {
+export const Register: React.FC = (): React.JSX.Element => {
+	const [nameError, setNameError] = useState("");
+
+	const validateName = (e: any) => {
+		if (e.target.value < 3) {
+			setNameError("Поля должны содержать не менее трех символов!");
+		} else {
+			setNameError("");
+		}
+	};
+
+	const RegisterFields: React.FC = (): React.JSX.Element => {
+		return (
+			<div className="register-fields">
+				<div className="inputs">
+					<input
+						type="text"
+						name="firstName"
+						className="input"
+						onBlur={validateName}
+						placeholder="First name"
+					/>
+					<input
+						type="text"
+						name="lastName"
+						className="input"
+						onBlur={validateName}
+						placeholder="Last name"
+					/>
+				</div>
+				{nameError && <span className="error">{nameError}</span>}
+			</div>
+		);
+	};
 
 	return (
 		<div className="register">
-			<div className="register__name">
-				<input
-					type="text"
-					defaultValue={state.firstName}
-					className={`register__name-input ${
-						state.error.firstName && "error"
-					}`}
-					name="firstName"
-					placeholder="First name"
-				/>
-				<input
-					type="text"
-					defaultValue={state.lastName}
-					className={`register__name-input ${
-						state.error.lastName && "error"
-					}`}
-					name="lastName"
-					placeholder="Last name"
-				/>
-			</div>
-			{(state.error.firstName || state.error.lastName) && (
-				<span className="text-error">
-					Поле не должно начинаться с цифры и содержать не менее трех
-					букв!
-				</span>
-			)}
-
-			<Login {...state} />
+			<Form
+				formClass=""
+				Component={RegisterFields}
+				childrenBtn="get started"
+			/>
 		</div>
 	);
 };

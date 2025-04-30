d930ab6e821ae0040c8bc35873448d67526f1a7c
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-05-01 01:45:48 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 				<Routes>
 					<Route path="/" element={<Login />}/>
 					<Route path="/register" element={<Register />} />
-					<Route path="main" element={<Main />} />
+					<Route path="/main" element={<Main />} />
 				</Routes>
 			</BrowserRouter>
 		</>

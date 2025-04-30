99f1b588bbea4c7ca3efc08a80229791d06e43f8
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-04-30 12:15:39 +0300

    fix

--- src/pages/auth/ui/login/Login.tsx
+++ src/pages/auth/ui/login/Login.tsx
@@ -1,17 +1,13 @@
 import React from "react";
-import { UserType } from "../../../../shared/types/types";
 import "./login.scss";
 import { Form } from "../../../../shared/components/form/Form";
 
 
 
-export const Login: React.FC<UserType> = (state): React.JSX.Element => {
+export const Login: React.FC = (): React.JSX.Element => {
 	return (
 		<>
 			<Form formClass="login" Component={null} childrenBtn="log in"/>
-			{(state.error.email || state.error.password) && (
-				<span className="error">Не правильно введен email или пароль</span>
-			)}
 		</>
 	);
 };

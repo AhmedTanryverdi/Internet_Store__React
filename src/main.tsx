023731f0330fa410b7cb76188ba452c9a5c4f6cf
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-05-02 02:51:24 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+import store from "./app/store/index.ts";
 import App from "./App.tsx";
 import "./index.css";
 
@@ -11,7 +13,9 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<QueryClientProvider client={queryClient}>
-			<App />
+			<Provider store={store}>
+				<App />
+			</Provider>
 		</QueryClientProvider>
 	</StrictMode>
 );

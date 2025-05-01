1de755b5eeb39c05cb9d19112e8854523a1db07b
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-05-02 02:58:10 +0300

    added component

---
+++ src/shared/components/form/ui/input/Input.tsx
@@ -0,0 +1,32 @@
+import React, { ChangeEvent, useState } from "react";
+import "./input.scss";
+
+export const Input: React.FC<{
+	name: string;
+	type: string;
+	placeholder: string;
+	defaultValue: string;
+	validateField: (str: string) => Boolean;
+}> = ({ name, type, placeholder, defaultValue, validateField }) => {
+	const [error, setError] = useState<Boolean>(false);
+
+	return (
+		<label>
+			<input
+				type={`${type}`}
+				name={`${name}`}
+				className="input"
+				placeholder={`${placeholder}`}
+				defaultValue={defaultValue}
+				onBlur={(e: ChangeEvent<HTMLInputElement>) =>
+					setError(validateField(e.target.value))
+				}
+			/>
+			{error && (
+				<span className="error">
+					Проверьте корректность введенных данных
+				</span>
+			)}
+		</label>
+	);
+};

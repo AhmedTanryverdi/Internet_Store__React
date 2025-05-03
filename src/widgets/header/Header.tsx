146b22ae78322965f1a458b0887511b16689abb4
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-05-03 10:00:18 +0300

    added header element

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./header.scss";
+import { Exit } from "./ui/exit/Exit";
+import { Filter } from "./ui/filter/Filter";
 
 export const Header: React.FC = (): React.JSX.Element => {
 	return (
@@ -7,6 +9,10 @@ export const Header: React.FC = (): React.JSX.Element => {
 			<div className="container">
 				<div className="content">
 					<h1 className="title">Лучшие товары</h1>
+					<div className="actions">
+                        <Filter />
+						<Exit />
+					</div>
 				</div>
 			</div>
 		</header>

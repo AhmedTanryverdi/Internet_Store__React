424e9c8ce014b119fee4a323e27fa76344367303
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-05-03 11:51:00 +0300

    added logic to filter component

--- src/widgets/header/ui/filter/Filter.tsx
+++ src/widgets/header/ui/filter/Filter.tsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
+import { CiSearch } from "react-icons/ci";
+import { IoIosClose } from "react-icons/io";
 import "./filter.scss";
 
 export const Filter: React.FC = (): React.JSX.Element => {
+	const [isFocused, setIsFocused] = useState(false);
+	const [value, setValue] = useState<string>("");
+
+	const handleChangeValue = (e: ChangeEvent<HTMLInputElement>) => {
+		setValue(e.target.value);
+	};
+
 	return (
 		<div className="filter-container">
-			<div className="filter__label">
-				<input type="text" id="filter" className="filter"/>
-			</div>
+			<label className="filter__label" data-focus={isFocused}>
+				<input
+					type="text"
+					id="filter"
+					value={value}
+					onChange={handleChangeValue}
+					onFocus={() => setIsFocused(true)}
+					onBlur={() => setIsFocused(false)}
+					className="filter"
+				/>
+				{(value !== "" && (
+					<IoIosClose size={17} color="red" onClick={()=>setValue("")} />
+				)) || <CiSearch size={17} color="green" onClick={()=>setIsFocused(true)}/>}
+			</label>
 		</div>
 	);
 };

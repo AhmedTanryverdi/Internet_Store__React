2b15772fbc2201d382b1be3d2c43360cfe23a24d
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-05-03 12:41:04 +0300

    added logic for exit

--- src/widgets/header/ui/exit/Exit.tsx
+++ src/widgets/header/ui/exit/Exit.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import "./exit.scss";
+import { useAppDispatch } from "../../../../shared/utils/types/types";
+import { setUser } from "../../../../entities/model/slices/userSlice";
 
 export const Exit: React.FC = (): React.JSX.Element => {
+	const dispatch = useAppDispatch();
 	return (
 		<div className="exit">
-			<button type="button" className="exit__btn">
+			<button
+				type="button"
+				className="exit__btn"
+				onClick={() => {
+					localStorage.clear();
+					dispatch(
+						setUser({
+							firstname: "",
+							lastname: "",
+							email: "",
+							password: "",
+						})
+					);
+				}}
+			>
 				Выйти
 			</button>
 		</div>

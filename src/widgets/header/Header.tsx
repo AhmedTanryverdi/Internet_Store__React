8f48c9a1845a037380ee9052f01d85b81b171cd4
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-05-03 12:40:24 +0300

    added logic for access

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,9 +1,19 @@
-import React from "react";
-import "./header.scss";
+import React, { useEffect } from "react";
 import { Exit } from "./ui/exit/Exit";
 import { Filter } from "./ui/filter/Filter";
+import { useSelector } from "react-redux";
+import { RootState, UserType } from "../../shared/utils/types/types";
+import { useNavigate } from "react-router-dom";
+import "./header.scss";
 
 export const Header: React.FC = (): React.JSX.Element => {
+	const navigate = useNavigate();
+	const user = useSelector<RootState, UserType>((state) => state.user.user);
+	useEffect(() => {
+		if (!user.email && !user.email) {
+			navigate("/");
+		}
+	}, [user]);
 	return (
 		<header className="header">
 			<div className="container">

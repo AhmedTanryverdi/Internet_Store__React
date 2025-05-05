6e8bb75acbf5ce5243cc25289ad99ee7d428a601
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-05-05 22:46:19 +0300

    refactoring

--- src/shared/components/form/Form.tsx
+++ src/shared/components/form/Form.tsx
@@ -1,19 +1,14 @@
-import React, { useActionState, useState } from "react";
+import React, { useEffect } from "react";
 import { Button } from "../button/Button";
 import { TopRow } from "./ui/top-row/TopRow";
 import { Input } from "./ui/input/Input";
-import { validate } from "./utils/functions";
-import {
-	IUser,
-	RootState,
-	useAppDispatch,
-	UserType,
-} from "../../utils/types/types";
+import { RootState, useAppDispatch, UserType } from "../../utils/types/types";
 import { useNavigate } from "react-router-dom";
-import { setUser } from "../../../entities/model/slices/userSlice";
 import { useSelector } from "react-redux";
 import { InputField } from "../../utils/constants";
+import { useCustomForm } from "./utils/hooks";
 import "./styles.scss";
+import { Title } from "./ui/title/Title";
 
 export const Form: React.FC<{
 	formClass: string;
@@ -21,70 +16,37 @@ export const Form: React.FC<{
 	childrenBtn: string;
 	url: string;
 }> = ({ formClass, Component, childrenBtn, url }): React.JSX.Element => {
-	const initialState = useSelector<RootState, IUser>((state) => state.user);
-
+	const initialState = useSelector<RootState, UserType>(
+		(state) => state.user.user
+	);
 	const dispatch = useAppDispatch();
 	const navigate = useNavigate();
-	const [error, setError] = useState(false);
-	const [message, setMessage] = useState<string>("");
 
-	const [state, formAction, isPending] = useActionState(
-		async (
-			initialState: Partial<UserType>,
-			formData: FormData
-		): Promise<Partial<UserType>> => {
-			const data: UserType = Object.fromEntries(
-				formData.entries()
-			) as UserType;
+	useEffect(() => {
+		const storageUser = localStorage.getItem("user");
 
-			const errorValidate = validate(data);
-
-			if (errorValidate) {
-				setError(errorValidate);
-				return data;
-			}
-
-			try {
-				const response = await fetch(`http://localhost:8000/${url}`, {
-					method: "POST",
-					headers: { "Content-Type": "application/json" },
-					body: JSON.stringify(data),
-				});
-
-				if (!response.ok) {
-					setError(true);
-					setMessage("Пользователь не найден!");
-					return data;
-				}
-				const result = await response.json();
-
-				dispatch(setUser({ ...result.user }));
-				localStorage.setItem("user", JSON.stringify(result));
-			} catch (error) {
-				setError(true);
-				setMessage("Непредвиденная ошибка!");
-				return data;
-			}
-
-			setError(false);
-			setMessage("");
-
+		if (storageUser !== null) {
 			navigate("/main");
-			return initialState;
-		},
-		initialState.user
+		}
+	}, []);
+
+	const [state, formAction, isPending, status] = useCustomForm(
+		initialState,
+		url,
+		dispatch
 	);
+	useEffect(() => {
+		if (status.ok) {
+			navigate("/main");
+		}
+	}, [status]);
 
 	return (
 		<div className="access">
 			<div className="container">
 				<form action={formAction} className={`form ${formClass}`}>
 					<TopRow />
-					<div className="form__header">
-						<div className="form__header-title">
-							<h2 className="title">Sign Up for Free</h2>
-						</div>
-					</div>
+					<Title />
 					{Component && <Component />}
 
 					{InputField.map((item, index: number) => {
@@ -99,7 +61,9 @@ export const Form: React.FC<{
 					})}
 
 					<Button children={childrenBtn} isPending={isPending} />
-					{error && <span className="error">{message}</span>}
+					{status.error && (
+						<span className="error">{status.message}</span>
+					)}
 				</form>
 			</div>
 		</div>

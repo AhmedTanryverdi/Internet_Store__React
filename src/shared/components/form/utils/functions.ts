db6bb4666a03d3abddc0146c461afa86ab0e255e
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-05-05 22:49:10 +0300

    refactoring

--- src/shared/components/form/utils/functions.ts
+++ src/shared/components/form/utils/functions.ts
@@ -28,3 +28,22 @@ export function validate(data: Object): boolean {
 	}
 	return error;
 }
+
+export async function fetchUser(url: string, data: Object) {
+	try {
+		const response = await fetch(`http://localhost:8000/${url}`, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(data),
+		});
+
+		if (!response.ok) {
+			return { data, status: response.status, ok: false };
+		}
+
+		const resultFetchingData = await response.json();
+		return { data: resultFetchingData, status: response.status, ok: true };
+	} catch (error) {
+		return { data, status: 500, ok: false };
+	}
+}

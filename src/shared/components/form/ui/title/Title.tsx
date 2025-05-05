794862ad0cdc61ddee7a5f52642a6bd231a777f7
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-05-05 22:41:30 +0300

    created title component

---
+++ src/shared/components/form/ui/title/Title.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./title.scss";
+
+export const Title: React.FC = (): React.JSX.Element => {
+	return (
+		<div className="form__header">
+			<div className="form__header-title">
+				<h2 className="title">Sign Up for Free</h2>
+			</div>
+		</div>
+	);
+};

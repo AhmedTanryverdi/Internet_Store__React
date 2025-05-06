5caecfc59ddad5cab32d0766f63631a9276caabb
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-05-06 20:42:06 +0300

    added intersection observer

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,26 +1,52 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef, useState } from "react";
 import { ProductType } from "../../shared/utils/types/types";
 import { Header } from "../../widgets/header/Header";
 import { Card } from "../../shared/components/card/Card";
+import { useScroll } from "./utils/hooks";
 import "./main.scss";
 
 export const Main: React.FC = (): React.JSX.Element => {
 	const [products, setProducts] = useState<ProductType[]>([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [skip, setSkip] = useState<number>(0);
+	const parentRef = useRef<HTMLDivElement>(null);
+	const childRef = useRef<HTMLDivElement>(null);
 
-	useEffect(() => {
-		fetch("https://dummyjson.com/products/")
-			.then((response) => response.json())
-			.then((data) => setProducts(data.products));
-	}, []);
+	useScroll(parentRef, childRef, isLoading, productFetching);
+
+	function productFetching() {
+		if (!isLoading) {
+			setIsLoading(true);
+			fetch(`https://dummyjson.com/products?limit=6&skip=${skip * 6}`)
+				.then((response) => response.json())
+				.then((data) => {
+					setProducts((prevProducts) => [
+						...prevProducts,
+						...data.products,
+					]);
+					setSkip(skip + 1);
+				})
+				.finally(() => setIsLoading(false));
+		}
+	}
 
 	return (
 		<div className="main">
 			<div className="container">
 				<Header />
-				<div className="products">
-					{products.map((product, index) => {
-						return <Card key={index} {...product} />;
+				<div className="products" ref={parentRef}>
+					{products.map((product) => {
+						return <Card key={product.id} {...product} />;
 					})}
+					<div
+						className="child"
+						ref={childRef}
+						style={{
+							width: "100%",
+							height: "20px",
+							background: "black",
+						}}
+					></div>
 				</div>
 			</div>
 		</div>

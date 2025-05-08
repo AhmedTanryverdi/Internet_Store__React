a1e72ebf2db39b6f86018901c61f1bc0cd4f4fee
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-05-08 16:37:51 +0300

    tanstack-query is deleted

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import store from "./app/store/index.ts";
 import App from "./App.tsx";
 import "./index.css";
 
-
-
-const queryClient = new QueryClient();
-
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<Provider store={store}>
-				<App />
-			</Provider>
-		</QueryClientProvider>
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</StrictMode>
 );

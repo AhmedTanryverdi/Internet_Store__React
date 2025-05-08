da8c396f9fc9971c11533357d2e4688e029512ed
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-05-09 01:25:28 +0300

    refactoring

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,52 +1,22 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import { ProductType, RootState } from "../../shared/utils/types/types";
+import React, { useRef } from "react";
+import { RootState } from "../../shared/utils/types/types";
 import { Header } from "../../widgets/header/Header";
 import { Card } from "../../shared/components/card/Card";
-import { useScroll } from "./utils/hooks";
+import { useProductFetch, useScroll } from "./utils/hooks";
 import loading from "../../shared/assets/icons/loading.png";
 import { useSelector } from "react-redux";
 import "./main.scss";
 
 export const Main: React.FC = (): React.JSX.Element => {
-	const [products, setProducts] = useState<ProductType[]>([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const [hasMore, setHasMore] = useState(true);
-	const skip = useRef<number>(0);
 	const childRef = useRef<HTMLDivElement>(null);
 	const filter = useSelector<RootState, string>(
 		(state) => state.filter.textInput
 	);
 
-	const productFetching = useCallback(() => {
-		if (!isLoading) {
-			setIsLoading(true);
-			fetch(
-				`https://dummyjson.com/products/search?q=${encodeURIComponent(
-					filter
-				)}&limit=18&skip=${skip.current * 18}`
-			)
-				.then((response) => response.json())
-				.then((data) => {
-					if (data.products.length < 18) {
-						setHasMore(false);
-					}
-					setProducts((prevProducts) => [
-						...prevProducts,
-						...data.products,
-					]);
-					skip.current += 1;
-				})
-				.finally(() => setIsLoading(false));
-		}
-	}, [isLoading, filter]);
+	const { products, isLoading, hasMore, fetchNextPage } =
+		useProductFetch(filter);
 
-	useScroll(childRef, isLoading, hasMore, productFetching);
-
-	useEffect(() => {
-		setProducts([]);
-		skip.current = 0;
-		setHasMore(true);
-	}, [filter]);
+	useScroll(childRef, isLoading, hasMore, fetchNextPage);
 
 	return (
 		<div className="main">
@@ -57,7 +27,13 @@ export const Main: React.FC = (): React.JSX.Element => {
 						return <Card key={product.id} {...product} />;
 					})}
 					<div className="child" ref={childRef}>
-						{hasMore && <img src={loading} className="img" alt="loading icon" />}
+						{hasMore && (
+							<img
+								src={loading}
+								className="img"
+								alt="loading icon"
+							/>
+						)}
 					</div>
 				</div>
 			</div>
--- src/pages/main/utils/hooks.ts
+++ src/pages/main/utils/hooks.ts
@@ -1,4 +1,5 @@
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { ProductType } from "../../../shared/utils/types/types";
 
 export const useScroll = (
 	childRef: React.RefObject<HTMLDivElement | null>,
@@ -35,4 +36,42 @@ export const useScroll = (
 			}
 		};
 	}, [callback, hasMore, isLoading]);
+};
+
+export const useProductFetch = (filter: string) => {
+	const [products, setProducts] = useState<ProductType[]>([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [hasMore, setHasMore] = useState(true);
+	const skip = useRef<number>(0);
+
+	const fetchNextPage = useCallback(() => {
+		if (!isLoading) {
+			setIsLoading(true);
+			fetch(
+				`https://dummyjson.com/products/search?q=${encodeURIComponent(
+					filter
+				)}&limit=18&skip=${skip.current * 18}`
+			)
+				.then((response) => response.json())
+				.then((data) => {
+					if (data.products.length < 18) {
+						setHasMore(false);
+					}
+					setProducts((prevProducts) => [
+						...prevProducts,
+						...data.products,
+					]);
+					skip.current += 1;
+				})
+				.finally(() => setIsLoading(false));
+		}
+	}, [isLoading, filter]);
+
+	useEffect(() => {
+		setProducts([]);
+		skip.current = 0;
+		setHasMore(true);
+	}, [filter]);
+
+	return { products, isLoading, hasMore, fetchNextPage };
 };

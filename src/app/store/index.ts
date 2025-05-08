2ff53fcb69a873f68ac14ba54a39b5e2aecd14ba
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-05-08 19:00:48 +0300

    added filter reducer

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import errorFormSlice from "../../entities/model/slices/errorFormSlice";
 import userSlice from "../../entities/model/slices/userSlice";
+import filterSlice from "../../entities/model/slices/filterSlice";
 
 const store = configureStore({
 	reducer: {
 		errorForm: errorFormSlice,
 		user: userSlice,
+		filter: filterSlice
 	},
 });
 

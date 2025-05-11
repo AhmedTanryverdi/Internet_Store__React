ddba0eb7f46e3c0bf22210382f426a21d324e259
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-05-12 01:55:44 +0300

    added buttons for counter

--- src/pages/main/ui/ui/description/DescriptionProduct.tsx
+++ src/pages/main/ui/ui/description/DescriptionProduct.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { StarRating } from "./star-rating/StarRating";
-import "./descriptionProduct.scss";
 import { Button } from "../../../../../shared/components/button/Button";
+import "./descriptionProduct.scss";
 
 export const DescriptionProduct: React.FC<{
 	title: string;
@@ -9,13 +9,25 @@ export const DescriptionProduct: React.FC<{
 	description: string;
 	rating: number;
 }> = ({ title, price, description, rating }): React.JSX.Element => {
+	const [count, setCount] = useState<number>(1);
 	return (
-		<div className="description">
+		<div className="descriptionBlock">
 			<h2 className="title">{title}</h2>
 			<StarRating rating={rating} />
 			<p className="price">${price}</p>
 			<p className="description">{description}</p>
-			<Button children="добавить в корзину" className="addToCard" />
+			<div className="descriptionBlock__btns">
+				<div className="counter">
+					<Button children="+" className="vary" onClick={()=>setCount(count+1)}/>
+					<div className="count">{count}</div>
+					<Button children="-" className="vary" disabled={count <= 1} onClick={()=>{
+						if(count > 1){
+							setCount(count - 1);
+						}
+					}}/>
+				</div>
+				<Button children="добавить в корзину" className="addToCard" />
+			</div>
 		</div>
 	);
 };

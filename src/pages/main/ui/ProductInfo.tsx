7e1292fa7bd94e49b3eef08476a2e6f64a61327d
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-05-11 15:24:36 +0300

    added ProductInfo component

---
+++ src/pages/main/ui/ProductInfo.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./productinfo.scss";
+
+export const ProductInfo: React.FC = (): React.JSX.Element => {
+	return <div className="productInfo">
+        <div className="container">
+            info
+        </div>
+    </div>;
+};

8e7f22cab7eebe77be09b47af1863a6ea6cf8f60
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-05-12 18:39:44 +0300

    added slice in common reducer

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -2,12 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import errorFormSlice from "../../entities/model/slices/errorFormSlice";
 import userSlice from "../../entities/model/slices/userSlice";
 import filterSlice from "../../entities/model/slices/filterSlice";
-
+import iconCartColor from "../../entities/model/slices/iconCartColor";
 const store = configureStore({
 	reducer: {
 		errorForm: errorFormSlice,
 		user: userSlice,
-		filter: filterSlice
+		filter: filterSlice,
+		iconCartColor: iconCartColor,
 	},
 });
 

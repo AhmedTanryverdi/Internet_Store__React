aa63ada34e3f75737fff2ae8c5258b1d4c853902
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-05-12 18:44:18 +0300

    added tracking for icon cart color

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -2,33 +2,50 @@ import React, { useEffect } from "react";
 import { IoCartOutline } from "react-icons/io5";
 import { Exit } from "./ui/exit/Exit";
 import { Filter } from "./ui/filter/Filter";
-import { useAppDispatch } from "../../shared/utils/types/types";
+import { RootState, useAppDispatch } from "../../shared/utils/types/types";
 import { useNavigate } from "react-router-dom";
 import { setUser } from "../../entities/model/slices/userSlice";
+import { useSelector } from "react-redux";
+import { setColor } from "../../entities/model/slices/iconCartColor";
 import "./header.scss";
 
 export const Header: React.FC = (): React.JSX.Element => {
 	const navigate = useNavigate();
 	const dispatch = useAppDispatch();
-
+	const iconCartColor = useSelector<RootState, string>(
+		(state) => state.iconCartColor.color
+	);
 	useEffect(() => {
+		const colorStorage = localStorage.getItem("cartProducts");
 		const storageUser = localStorage.getItem("user");
+
+		if (colorStorage) {
+			dispatch(setColor("green"));
+		}
 
 		if (storageUser !== null) {
 			dispatch(setUser(JSON.parse(storageUser)));
 		} else {
 			navigate("/");
 		}
-	}, []);
+	}, [iconCartColor]);
 
 	return (
 		<header className="header">
 			<div className="container">
 				<div className="content">
-					<h1 className="title" onClick={()=>navigate("/")}>Лучшие товары</h1>
+					<h1 className="title" onClick={() => navigate("/")}>
+						Лучшие товары
+					</h1>
 					<div className="actions">
 						<Filter />
-						<IoCartOutline color="blue" size={28}/>
+						<IoCartOutline
+							color={
+								(iconCartColor === "green" && "green") || "blue"
+							}
+							size={28}
+							onClick={() => navigate("/cart")}
+						/>
 						<Exit />
 					</div>
 				</div>

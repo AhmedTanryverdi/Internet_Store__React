6a944910dad0546c333128d45ffe176d60d41deb
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-05-12 23:12:32 +0300

    added type

--- src/shared/utils/types/types.ts
+++ src/shared/utils/types/types.ts
@@ -34,6 +34,10 @@ export type ProductType = {
 	images: string[];
 };
 
+export interface localStorageType extends ProductType {
+	quentity: number;
+}
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();

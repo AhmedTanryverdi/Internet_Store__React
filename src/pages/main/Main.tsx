e65655aa8125473bde90ab62a096d7c7f66256ea
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-05-12 18:41:20 +0300

    added logic for icon cart color

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,15 +1,18 @@
 import React, { useRef } from "react";
-import { RootState } from "../../shared/utils/types/types";
+import { RootState, useAppDispatch } from "../../shared/utils/types/types";
 import { Card } from "../../shared/components/card/Card";
 import { useProductFetch, useScroll } from "./utils/hooks";
 import loading from "../../shared/assets/icons/loading.png";
 import { useSelector } from "react-redux";
 import { addToCart } from "../../shared/utils/functions/functions";
 import "./main.scss";
+import { setColor } from "../../entities/model/slices/iconCartColor";
 
 
 export const Main: React.FC = (): React.JSX.Element => {
 	const childRef = useRef<HTMLDivElement>(null);
+	const dispatch = useAppDispatch();
+	
 	const filter = useSelector<RootState, string>(
 		(state) => state.filter.textInput
 	);
@@ -30,6 +33,7 @@ export const Main: React.FC = (): React.JSX.Element => {
 								{...product}
 								onClick={() => {
 									addToCart(product);
+									dispatch(setColor("green"));
 								}}
 							/>
 						);
